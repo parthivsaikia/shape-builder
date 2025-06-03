c1923c00f779c9a62096ad123d519c8552a41d02
mahadevan <135952571+M-DEV-1@users.noreply.github.com>
2025-06-03 18:29:54 +0530

    Chore: Update sistent package

--- site/src/components/ShapeBuilder/index.js
+++ site/src/components/ShapeBuilder/index.js
@@ -1,7 +1,7 @@
 // /* global window */
 import React, { useEffect, useRef, useState } from "react";
 import { Wrapper, CanvasContainer, OutputBox, StyledSVG } from "./shapeBuilder.styles";
-import { Button, Typography, Box } from "@layer5/sistent";
+import { Button, Typography, Box } from "@sistent/sistent";
 import { SVG, extend as SVGextend } from "@svgdotjs/svg.js";
 import draw from "@svgdotjs/svg.draw.js";
 
